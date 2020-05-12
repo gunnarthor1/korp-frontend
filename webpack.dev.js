b009308f61c8ee17c4fc81383a3a97cf8d3c415d
G-Thor <ornolfsson@gmail.com>
2020-05-12 13:04:00 +0000

    make dev server accept remote connections

--- webpack.dev.js
+++ webpack.dev.js
@@ -4,7 +4,7 @@ const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
   devServer: {
-    host: process.env.KORP_HOST || "localhost",
+    host: process.env.KORP_HOST || "0.0.0.0",
     port: process.env.KORP_PORT || 9111
   },
   devtool: 'inline-source-map',
